08c8f8c429de2392a1c5fe3b18447f2e00788b25
anderson.boechat <anderson.boechat@SPW5194BTW7P.sondait.com.br>
2015-03-12 15:14:26 -0300

    .

--- jcondo-api/src/br/com/abware/jcondo/core/model/Kinship.java
+++ jcondo-api/src/br/com/abware/jcondo/core/model/Kinship.java
@@ -4,16 +4,35 @@ public class Kinship implements BaseModel {
 
 	private long id;
 	
+	private Person person;
+	
 	private Person relative;
 	
 	private KinType type;
-	
+
+	public Kinship() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Kinship(Person person, Person relative, KinType type) {
+		this.relative = relative;
+		this.type = type;
+	}
+
 	public long getId() {
 		return id;
 	}
 
 	public void setId(long id) {
 		this.id = id;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
 	}
 
 	public Person getRelative() {
