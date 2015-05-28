2c6e3e23a3d46e4cf77cc183075c183660465d31
anderson.boechat <anderson.boechat@SPW5194BTW7P.sondait.com.br>
2015-05-28 20:47:17 -0300

    .

--- jcondo-api/src/br/com/abware/jcondo/booking/model/RoomBooking.java
+++ jcondo-api/src/br/com/abware/jcondo/booking/model/RoomBooking.java
@@ -1,11 +1,14 @@
 package br.com.abware.jcondo.booking.model;
 
 import java.util.Date;
+import java.util.List;
 
 import br.com.abware.jcondo.core.model.Domain;
 import br.com.abware.jcondo.core.model.Person;
 
 public class RoomBooking extends Booking<Room> {
+	
+	private List<Person> guests;
 
 	public RoomBooking() {
 		super();
@@ -13,6 +16,14 @@ public class RoomBooking extends Booking<Room> {
 
 	public RoomBooking(Person person, Domain domain, Room room, Date beginDate, Date endDate) {
 		super(person, domain, room, beginDate, endDate);
+	}
+
+	public List<Person> getGuests() {
+		return guests;
+	}
+
+	public void setGuests(List<Person> guests) {
+		this.guests = guests;
 	}
 
 }
